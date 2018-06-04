f421a416bf3a93a77ec2bba1bf1f738bf7763246
VM IUT <vm@iut>
2018-06-04 11:42:18 +0200

    erreur segmentation

---
+++ SegFault/SegmentationFault.java
@@ -0,0 +1,12 @@
+public class SegmentationFault{
+	
+	public static void main (String[] args){
+		int t[] = new int [10];
+		t[100] = 12;
+		t[1000] = 123;
+		t[10000] = 1234;
+		t[100000] = 12345;
+		t[1000000] = 123456;	
+
+	}
+}
